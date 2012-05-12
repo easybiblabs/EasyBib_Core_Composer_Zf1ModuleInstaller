1b0e5fb9ac996436cfc2546013d25daec12772e4
till <till@php.net>
2012-05-12 19:47:30 +0200

    test our installer

---
+++ tests/EasyBib/Core/Composer/Test/Zf1ModuleInstallerTestCase.php
@@ -0,0 +1,51 @@
+<?php
+namespace EasyBib\Core\Composer\Test;
+
+use EasyBib\Core\Composer\Zf1ModuleInstaller;
+use Composer\Downloader\DownloadManager;
+use Composer\IO\NullIO;
+
+class Zf1ModuleInstallerTestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testRuntimeException()
+    {
+        new Zf1ModuleInstaller(
+            'vendor',
+            'bin',
+            new DownloadManager(),
+            new NullIO()
+        );
+    }
+
+    public static function envProvider()
+    {
+        return array(
+            array('test1', 'app/modules'),
+            array('test2', 'application/modules'),
+        );
+    }
+
+    /**
+     * @dataProvider envProvider
+     */
+    public function testCorrectVendorDir($env, $assertion)
+    {
+        $fixtureDir = dirname(dirname(dirname(dirname(__DIR__)))) . '/fixtures';
+
+        $currentCwd = getcwd();
+        chdir($fixtureDir . '/' . $env);
+
+        $installer = new Zf1ModuleInstaller(
+            'vendor',
+            'bin',
+            new DownloadManager(),
+            new NullIO()
+        );
+        $this->assertEquals($assertion, $installer->getVendorDir());
+
+        chdir($currentCwd);
+    }
+}
