2c68113d434a5abd62476b9c2231ae4fdb479a64
till <till@php.net>
2012-05-13 00:48:58 +0200

    test case to strip vendor

--- tests/EasyBib/Core/Composer/Test/Zf1ModuleInstallerTestCase.php
+++ tests/EasyBib/Core/Composer/Test/Zf1ModuleInstallerTestCase.php
@@ -4,6 +4,7 @@ namespace EasyBib\Core\Composer\Test;
 use EasyBib\Core\Composer\Zf1ModuleInstaller;
 use Composer\Downloader\DownloadManager;
 use Composer\IO\NullIO;
+use Composer\Package\MemoryPackage;
 
 /**
  * @author Till Klampaeckel <till@lagged.biz>
@@ -48,6 +49,27 @@ class Zf1ModuleInstallerTestCase extends \PHPUnit_Framework_TestCase
             new NullIO()
         );
         $this->assertEquals($assertion, $installer->getVendorDir());
+
+        chdir($currentCwd);
+    }
+
+    public function testEnsureVendorIsStrippedFromInstallPath()
+    {
+        $fixtureDir = dirname(dirname(dirname(dirname(__DIR__)))) . '/fixtures';
+
+        $currentCwd = getcwd();
+        chdir($fixtureDir . '/test1');
+
+        $installer = new Zf1ModuleInstaller(
+            'vendor',
+            'bin',
+            new DownloadManager(),
+            new NullIO()
+        );
+
+        $package = new MemoryPackage('easybib-foo/default', '1.0.0', '1.0.0-stable');
+
+        $this->assertEquals('app/modules/default', $installer->getInstallPath($package));
 
         chdir($currentCwd);
     }
