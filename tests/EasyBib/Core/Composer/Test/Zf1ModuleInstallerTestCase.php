d9b75b50d6638dd2dd580e34db371b1c9c4fbbbd
till <till@php.net>
2012-05-17 20:29:53 +0200

    * test to confirm that extracting a module from another application works

--- tests/EasyBib/Core/Composer/Test/Zf1ModuleInstallerTestCase.php
+++ tests/EasyBib/Core/Composer/Test/Zf1ModuleInstallerTestCase.php
@@ -3,14 +3,24 @@ namespace EasyBib\Core\Composer\Test;
 
 use EasyBib\Core\Composer\Zf1ModuleInstaller;
 use Composer\Downloader\DownloadManager;
+use Composer\Downloader\SvnDownloader;
 use Composer\IO\NullIO;
 use Composer\Package\MemoryPackage;
+use Composer\Repository\ArrayRepository;
+use Composer\Repository\InstalledArrayRepository;
 
 /**
  * @author Till Klampaeckel <till@lagged.biz>
  */
 class Zf1ModuleInstallerTestCase extends \PHPUnit_Framework_TestCase
 {
+    protected $fixtureDir;
+
+    public function setUp()
+    {
+        $this->fixtureDir = dirname(dirname(dirname(dirname(__DIR__)))) . '/fixtures';
+    }
+
     public static function envProvider()
     {
         return array(
@@ -24,10 +34,8 @@ class Zf1ModuleInstallerTestCase extends \PHPUnit_Framework_TestCase
      */
     public function testCorrectVendorDir($env, $assertion)
     {
-        $fixtureDir = dirname(dirname(dirname(dirname(__DIR__)))) . '/fixtures';
-
         $currentCwd = getcwd();
-        chdir($fixtureDir . '/' . $env);
+        chdir($this->fixtureDir . '/' . $env);
 
         $installer = new Zf1ModuleInstaller(
             'vendor',
@@ -53,10 +61,8 @@ class Zf1ModuleInstallerTestCase extends \PHPUnit_Framework_TestCase
      */
     public function testEnsureVendorIsStrippedFromInstallPath($dep, $module)
     {
-        $fixtureDir = dirname(dirname(dirname(dirname(__DIR__)))) . '/fixtures';
-
         $currentCwd = getcwd();
-        chdir($fixtureDir . '/test1');
+        chdir($this->fixtureDir . '/test1');
 
         $installer = new Zf1ModuleInstaller(
             'vendor',
@@ -68,6 +74,52 @@ class Zf1ModuleInstallerTestCase extends \PHPUnit_Framework_TestCase
         $package = new MemoryPackage($dep, '1.0.0', '1.0.0-stable');
 
         $this->assertEquals('app/modules/' . $module, $installer->getInstallPath($package));
+
+        chdir($currentCwd);
+    }
+
+    /**
+     * The objective of this test is to confirm that the following works:
+     *
+     * 1) A SVN repository is used which contains a complete ZendFramework application.
+     * 2) We are trying to detect the application structure (application/modules or app/modules).
+     * 3) When detected, we adjust the check out URL so only the module is checked out.
+     *
+     * @return void
+     */
+    public function testInstallTransformations()
+    {
+        $this->markTestIncomplete("Needs some sort of mocking (works with real server and credentials).");
+
+        $currentCwd = getcwd();
+        chdir($this->fixtureDir . '/test1');
+
+        $io = new NullIO();
+
+        $downloadManager = new DownloadManager();
+        $downloadManager->setDownloader('svn', new SvnDownloader($io));
+
+        $installer = new Zf1ModuleInstaller(
+            'vendor',
+            'bin',
+            $downloadManager,
+            $io
+        );
+
+        /**
+         * @desc Build up a package to run this test against!
+         */
+        $package = new MemoryPackage('company/module-name', '1.3.6', '1.3.6');
+        $package->setSourceUrl('http://svn.company.tld/svn/project');
+        $package->setSourceReference('tags/1.3.6');
+        $package->setSourceType('svn');
+
+        $localRepository = new InstalledArrayRepository();
+
+        $installer->install($localRepository, $package);
+
+        $this->assertTrue(is_dir('app/modules/module-name'));
+        `rm -rf app/modules/module-name`;
 
         chdir($currentCwd);
     }
